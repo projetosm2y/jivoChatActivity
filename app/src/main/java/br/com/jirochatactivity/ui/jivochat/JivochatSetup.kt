fc145e3913c1f0b93c8316437e3edf1302c93b01
Golias <joao.victor@mobile2you.com.br>
2019-08-15 08:54:38 -0300

    set setup variables as open

--- app/src/main/java/br/com/jirochatactivity/ui/jivochat/JivochatSetup.kt
+++ app/src/main/java/br/com/jirochatactivity/ui/jivochat/JivochatSetup.kt
@@ -3,7 +3,7 @@ package br.com.jirochatactivity.ui.jivochat
 import java.io.Serializable
 
 open class JivochatSetup : Serializable{
-    var language: String = "pt"
+    open var language: String = "pt"
 
-    fun onEvent(name: String?, data: String?){}
+    open fun onEvent(name: String?, data: String?){}
 }
